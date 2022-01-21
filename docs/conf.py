60924c5a60b5187f58ccdb430d7ef6cf5d3f9e9f
David Wright <davecwright@knights.ucf.edu>
2022-01-21 15:32:10 -0500

    Update sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -6,5 +6,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
 ]
 html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
