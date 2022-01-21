9b9f87bc8afc29d5ad80ddfdc8f37ccf55a0cf9d
David Wright <davecwright@knights.ucf.edu>
2022-01-21 15:40:11 -0500

    Add docs to tests

--- tests/test_event_horyzen.py
+++ tests/test_event_horyzen.py
@@ -1,5 +1,7 @@
+"""Test event_horyzen."""
 from event_horyzen import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """Test version."""
+    assert __version__ == "0.1.2"
